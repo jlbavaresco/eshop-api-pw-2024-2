6317ac17df1674fef6c3e4d6b6330e993af3aed8
Jorge Luis Boeira Bavaresco <jorge@jorge.ifsul>
2024-11-25 09:11:21 -0300

    seguranca com jwt

--- routes/rotas.js
+++ routes/rotas.js
@@ -2,10 +2,15 @@ const { Router } = require('express');
 
 const { rotasCategorias } = require('./rotasCategorias');
 const { rotasProdutos } = require('./rotasProdutos');
+const { login } = require('../controllers/segurancaController');
 
 const rotas = new Router();
 
 rotas.use(rotasCategorias);
 rotas.use(rotasProdutos);
+
+rotas.route("/login")
+   .post(login)           
+	 
 
 module.exports = rotas;
